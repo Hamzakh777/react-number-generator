7e523cd1131382048155daad756cac10132c4422
Hamza <hamzabitup@gmail.com>
2020-04-30 18:05:08 +0000

    Refractor: making the card components reusable

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = () => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Start a new game!</Text>
-			<View style={styles.inputContainer}>
+			<Card style={styles.inputContainer}>
 				<Text>Select a number</Text>
 				<TextInput />
 				<View style={styles.btnContainer}>
 					<Button title="reset" onPress={() => {}} />
 					<Button title="Confirm" onPress={() => {}} />
 				</View>
-			</View>
+			</Card>
 		</View>
 	);
 };
@@ -23,24 +24,17 @@ const styles = StyleSheet.create({
 		padding: 10,
 		alignItems: 'center',
 	},
-	inputContainer: {
-        alignItems: 'center',
-        width: 300,
-        maxWidth: '80%',
-        padding: 20,
-        borderRadius: 8,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        elevation: 5,
-        backgroundColor: 'white'
-	},
 	btnContainer: {
 		flexDirection: 'row',
 		width: '100%',
 		justifyContent: 'space-between',
 		paddingHorizontal: 15,
+	},
+	inputContainer: {
+		alignItems: 'center',
+		width: 300,
+		maxWidth: '80%',
+		padding: 20,
 	},
 	title: {
 		fontSize: 20,
