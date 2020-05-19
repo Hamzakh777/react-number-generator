8e0e515854c3bd1de8d3b3a8088a7abef9530b6a
Hamza <hamzabitup@gmail.com>
2020-05-19 22:43:32 +0000

    Feat: game screen

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGagmeScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+
+// we need a way to switch between screens
 
 function App() {
   return (
