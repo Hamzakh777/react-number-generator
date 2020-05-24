6c29a646ae44737cc1cada938568f449a6f1bfb4
Hamza <hamzabitup@gmail.com>
2020-05-24 22:15:34 +0000

    Add style to the GameScreen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useState, FunctionComponent } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
 
 type TGameScreenProps = { 
     userChoice: number
@@ -18,7 +20,33 @@ const generateRandomNumber = (min: number, max: number, exclude: number): number
 
 const GameScreen: FunctionComponent<TGameScreenProps> = (props) => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, props.userChoice))
-    return <View></View>
+    return (
+		<View style={styles.screen}>
+            <Text>Opponent's guess</Text>
+            <NumberContainer>
+                {currentGuess}
+            </NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" onPress={() => {}}/>
+                <Button title="GREATER" onPress={() => {}}/>
+            </Card>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 10,
+		alignItems: 'center',
+	},
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
+});
 
 export default GameScreen;
