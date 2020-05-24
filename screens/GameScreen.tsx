06a071c7dee6cd9295b939c14979562860d63939
Hamza <hamzabitup@gmail.com>
2020-05-24 21:52:16 +0000

    Feat: generate a random number

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, FunctionComponent } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const GameScreen = () => {
+type TGameScreenProps { 
+    userChoice: number
+}
+
+const generateRandomNumber = (min: number, max: number, exclude: number): number => {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    const rndNum = Math.floor(Math.random() * (max - min) + min);
+    if ( rndNum === exclude ) {
+        return generateRandomNumber(min, max, exclude);
+    }
+
+    return rndNum;
+}
+
+const GameScreen: FunctionComponent<TGameScreenProps> = (props) => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, props.userChoice))
     return <View></View>
 }
 
