ee52eba7f2add4be379f31feb1522da0c8ce4cb1
Hamza <hamzabitup@gmail.com>
2020-05-26 12:50:01 +0000

    Feat: hints and validations

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,38 +1,82 @@
-import React, { useState, FunctionComponent } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState,useRef, FunctionComponent } from 'react';
+import { View, Text, Button, StyleSheet, Alert } from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 
-type TGameScreenProps = { 
-    userChoice: number
+type TGameScreenProps = {
+	userChoice: number;
+};
+
+enum Direction {
+	lower,
+	higher,
 }
 
-const generateRandomNumber = (min: number, max: number, exclude: number): number => {
-    min = Math.ceil(min);
-    max = Math.ceil(max);
-    const rndNum = Math.floor(Math.random() * (max - min) + min);
-    if ( rndNum === exclude ) {
-        return generateRandomNumber(min, max, exclude);
-    }
+const generateRandomNumber = (
+	min: number,
+	max: number,
+	exclude: number
+): number => {
+	min = Math.ceil(min);
+	max = Math.ceil(max);
+	const rndNum = Math.floor(Math.random() * (max - min) + min);
+	if (rndNum === exclude) {
+		return generateRandomNumber(min, max, exclude);
+	}
 
-    return rndNum;
-}
+	return rndNum;
+};
 
 const GameScreen: FunctionComponent<TGameScreenProps> = (props) => {
-    const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, props.userChoice))
-    return (
+	const [currentGuess, setCurrentGuess] = useState(
+		generateRandomNumber(1, 100, props.userChoice)
+    );
+    
+    // useRef allows to save a value even if the component re-renders
+    // also we don't want to re-render the component by changing something that
+    // has nothing to do with the state
+    const currentLow = useRef(1);
+    const currentHigh = useRef(2);
+
+	const nextGuessHandler = (direction: number): void => {
+		if (
+			(direction === Direction.lower &&
+				currentGuess < props.userChoice) ||
+			(direction === Direction.higher && currentGuess > props.userChoice)
+		) {
+			Alert.alert("Don't lie!!", 'You know that this is wrong....', [
+				{ text: 'Sorry', style: 'cancel' },
+            ]);
+
+            return;
+        }
+        // generate a new number
+        if (direction === Direction.lower) {
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomNumber(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextNumber);
+	};
+
+	return (
 		<View style={styles.screen}>
-            <Text>Opponent's guess</Text>
-            <NumberContainer>
-                {currentGuess}
-            </NumberContainer>
-            <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={() => {}}/>
-                <Button title="GREATER" onPress={() => {}}/>
-            </Card>
+			<Text>Opponent's guess</Text>
+			<NumberContainer>{currentGuess}</NumberContainer>
+			<Card style={styles.buttonContainer}>
+				<Button
+					title="LOWER"
+					onPress={() => nextGuessHandler(Direction.lower)}
+				/>
+				<Button
+					title="GREATER"
+					onPress={() => nextGuessHandler(Direction.higher)}
+				/>
+			</Card>
 		</View>
 	);
-}
+};
 
 const styles = StyleSheet.create({
 	screen: {
@@ -40,14 +84,14 @@ const styles = StyleSheet.create({
 		padding: 10,
 		alignItems: 'center',
 	},
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginTop: 20,
-        padding: 20,
-        width: 300,
-        maxWidth: '80%'
-    }
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginTop: 20,
+		padding: 20,
+		width: 300,
+		maxWidth: '80%',
+	},
 });
 
 export default GameScreen;
