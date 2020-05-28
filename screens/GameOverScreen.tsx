461361324fa9b72430b42fb8fe016e98d3ddbf22
Hamza <hamzabitup@gmail.com>
2020-05-28 12:59:20 +0000

    styling the image on GameOverScren

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -11,7 +11,13 @@ const GameOverScreen: FunctionComponent<TGameOverScreen> = (props) => {
 	return (
 		<View style={styles.screen}>
 			<Text>The Game is over</Text>
-			<Image source={require('../assets/success.png')}/>
+			<View style={styles.imageContainer}>
+				<Image
+					style={styles.image}
+					source={require('../assets/success.png')}
+					resizeMode="cover"
+				/>
+			</View>
 			<Text>Number of rounds: {props.roundsNumber}</Text>
 			<Text>Number was: {props.userNumber}</Text>
 			<Button title="New Game" onPress={props.onStartNewGame} />
@@ -24,6 +30,19 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		marginVertical: 30,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: 'black',
+		overflow: 'hidden'
 	},
 });
 
